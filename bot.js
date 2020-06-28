531c3cfaa13ec05b257e856ca501b39569123068
IceDops <johannesnydahl1@gmail.com>
2020-06-29 00:01:08 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config.json')
+
+
+
+client.on('message', msg => {
+    for (i in config.reactions) {
+        if (msg.content === config.reactions[i].message) {
+            msg.react(config.reactions[i].reactionEmoji)
+            .catch(console.error);
+        }
+    }
+});
+
+client.on('ready', () => {
+  console.log(`Bot is up and running`);
+});
+
+client.login(config.token);
